efff7a083bd000f02f7db3010abe0b5e84226df5
YinHangCode <141564@qq.com>
2018-01-23 21:55:56 +0800

    0.0.2

---
+++ ikonkeIO.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+basepath="$(cd `dirname $0`; pwd)"
+export LD_LIBRARY_PATH="$basepath"
+cd "$basepath"
+./ikonkeIO $@
--- ikonkeIO/ikonkeIO.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-basepath=$(cd `dirname $0`; pwd);
-export LD_LIBRARY_PATH=$basepath
-cd $basepath
-./ikonkeIO $@
